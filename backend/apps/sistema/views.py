a3a72e5f02155020ab1da0ed0be923bd0ef11dd5
j0rbe3n <jero.guzman@gmail.com>
2023-08-29 00:19:59 -0700

    add frontend and donations list endpoint

--- backend/apps/sistema/views.py
+++ backend/apps/sistema/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from .models import *
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class DonacionesView(APIView):
+    def get(self, request):
+        donaciones = Donaciones.objects.all()
+        list = []
+
+        for donacion in donaciones:
+            list.append({
+                'id': donacion.id,
+                'red': donacion.red.nombre,
+                'donador': donacion.donador.first_name + ' ' + donacion.donador.last_name,
+                'beneficiario': donacion.beneficiario.first_name + ' ' + donacion.beneficiario.last_name,
+                'fecha': donacion.fecha,
+                'evidencia': donacion.evidencia.url if donacion.evidencia else None,
+            })
+
+        return Response(list, status=status.HTTP_200_OK)
