d712e9f41a637fb15d190de9b89bca1fe0da7776
Finanzasparahippies <saul.redesdemercadeo@gmail.com>
2023-08-28 20:48:16 -0700

    upload project

---
+++ backend/apps/users/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import UserManager
+from phonenumber_field.modelfields import PhoneNumberField
+
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone = PhoneNumberField()
+    profile_img = models.ImageField(upload_to="photos", null=True, blank=True)
+    rfc = models.CharField(max_length=100)
+    bank_card = models.CharField(max_length=100)
+    bank_account = models.CharField(max_length=100)
+    bank_clabe = models.CharField(max_length=100)
+    terms = models.BooleanField(default=False)
+    email = models.EmailField(max_length=100, unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+    def _str_(self):
+        return f'{self.email}'
+    
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    class Meta:
+        ordering = ['-id']
+        verbose_name = 'Usuario'
+        verbose_name_plural = 'Usuarios'
+
