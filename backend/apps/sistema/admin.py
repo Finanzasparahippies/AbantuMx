dfdd05f16749f87a0dec11bb1efd407f865cd264
j0rbe3n <jero.guzman@gmail.com>
2023-09-07 16:45:17 -0700

    Modelo Suscripciones

--- backend/apps/sistema/admin.py
+++ backend/apps/sistema/admin.py
@@ -4,5 +4,6 @@ from .models import *
 admin.site.register(Redes)
 admin.site.register(Donaciones)
 admin.site.register(DonacionRevision)
+admin.site.register(Suscripcion)
 
 
--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -6,4 +6,5 @@ app_name = 'apps.users'
 urlpatterns = [
     path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('check-token/', checkToken.as_view(), name='checkToken'),
+    path('create-suscripcion/', CreateSuscripcion.as_view(), name='createSuscripcion'),
 ]
