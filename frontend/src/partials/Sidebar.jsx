23e0aac0620dca8c8033897adf5be67ca295ddbe
j0rbe3n <jero.guzman@gmail.com>
2024-03-06 15:59:29 -0700

    New build

--- frontend/src/partials/Sidebar.jsx
+++ frontend/src/partials/Sidebar.jsx
@@ -16,38 +16,38 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
     storedSidebarExpanded === null ? false : storedSidebarExpanded === "true"
   );
 
-  // useEffect(() => {
-  //   const clickHandler = ({ target }) => {
-  //     if (!sidebar.current || !trigger.current) return;
-  //     if (
-  //       !sidebarOpen ||
-  //       sidebar.current.contains(target) ||
-  //       trigger.current.contains(target)
-  //     )
-  //       return;
-  //     setSidebarOpen(false);
-  //   };
-  //   document.addEventListener("click", clickHandler);
-  //   return () => document.removeEventListener("click", clickHandler);
-  // });
+  useEffect(() => {
+    const clickHandler = ({ target }) => {
+      if (!sidebar.current || !trigger.current) return;
+      if (
+        !sidebarOpen ||
+        sidebar.current.contains(target) ||
+        trigger.current.contains(target)
+      )
+        return;
+      setSidebarOpen(false);
+    };
+    document.addEventListener("click", clickHandler);
+    return () => document.removeEventListener("click", clickHandler);
+  });
 
-  // useEffect(() => {
-  //   const keyHandler = ({ keyCode }) => {
-  //     if (!sidebarOpen || keyCode !== 27) return;
-  //     setSidebarOpen(false);
-  //   };
-  //   document.addEventListener("keydown", keyHandler);
-  //   return () => document.removeEventListener("keydown", keyHandler);
-  // });
+  useEffect(() => {
+    const keyHandler = ({ keyCode }) => {
+      if (!sidebarOpen || keyCode !== 27) return;
+      setSidebarOpen(false);
+    };
+    document.addEventListener("keydown", keyHandler);
+    return () => document.removeEventListener("keydown", keyHandler);
+  });
 
-  // useEffect(() => {
-  //   localStorage.setItem("sidebar-expanded", sidebarExpanded);
-  //   if (sidebarExpanded) {
-  //     document.querySelector("body").classList.add("sidebar-expanded");
-  //   } else {
-  //     document.querySelector("body").classList.remove("sidebar-expanded");
-  //   }
-  // }, [sidebarExpanded]);
+  useEffect(() => {
+    localStorage.setItem("sidebar-expanded", sidebarExpanded);
+    if (sidebarExpanded) {
+      document.querySelector("body").classList.add("sidebar-expanded");
+    } else {
+      document.querySelector("body").classList.remove("sidebar-expanded");
+    }
+  }, [sidebarExpanded]);
 
   return (
     <div>
